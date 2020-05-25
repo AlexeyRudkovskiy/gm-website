1953de8c1914cf247af337b1c01ea40b499f8971
AlexeyRudkovskiy <alex.rudkowskij@yandex.ru>
2020-05-25 11:21:35 +0300

    Added popup

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -11,6 +11,17 @@
     window.addEventListener('load', () => {
         const projectsAndProducts = document.querySelectorAll('.grid-item');
         const contentMargin = 20;
+        const popup = document.querySelector('.popup-container');
+        const popupContent = popup.querySelector('.popup-content .content');
+        const popupClose = popup.querySelector('.popup-header .close-button');
+        let popupImage = popup.querySelector('.popup-header img');
+
+        popupClose.addEventListener('click', () => {
+            popupContent.innerHTML = '';
+            popup.classList.add('hidden');
+            document.body.classList.remove('no-scroll');
+        });
+
         for (let i = 0; i < projectsAndProducts.length; i++) {
             const item = projectsAndProducts[i];
             const content = item.querySelector('.content');
@@ -27,6 +38,25 @@
             item.addEventListener('mouseleave', () => {
                 content.style.marginTop = `-${defaultMargin}px`;
             });
+
+            if (item.hasAttribute('data-open-popup')) {
+                item.addEventListener('click', () => {
+                    const content = item.querySelector('[data-popup-content]');
+                    const title = item.querySelector('.content h2').innerText;
+                    const contentBody = content.querySelector('.body');
+                    const image = content.querySelector('img[popup-header-image]');
+
+                    const newImage = image.cloneNode();
+
+                    document.body.classList.add('no-scroll');
+
+                    popupContent.innerHTML = contentBody.innerHTML;
+                    popupImage.parentElement.replaceChild(newImage, popupImage);
+                    popupImage = newImage;
+
+                    popup.classList.remove('hidden');
+                });
+            }
         }
     });
 
