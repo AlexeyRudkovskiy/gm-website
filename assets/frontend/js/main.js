88b889331555f173edced18c5aee1d373452db5a
AlexeyRudkovskiy <alex.rudkowskij@yandex.ru>
2020-06-09 18:20:21 +0300

    Added slider

--- assets/frontend/js/main.js
+++ assets/frontend/js/main.js
@@ -24,7 +24,7 @@ import axios from 'axios'
         const popupClose = popup.querySelector('.popup-header .close-button');
         const contactForm = document.querySelector('#contact-form');
         const slider = document.querySelector('.header-image-wrapper');
-        const sliderImage = slider.querySelector('img');
+        const sliderImage = slider.querySelector('.header-image');
         const slidesJson = slider.getAttribute('data-images');
         const slides = JSON.parse(slidesJson);
         const totalSlides = slides.length;
@@ -74,17 +74,29 @@ import axios from 'axios'
             }
         }
 
+        const getImageUrl = (filename) => {
+            return `/uploads/photos/${filename}`;
+        };
+
         window.setInterval(() => {
             currentSlide++;
             if (currentSlide >= totalSlides) {
                 currentSlide = 0;
             }
 
-            const defaultImage = 'slider-large';
             const data = slides[currentSlide];
 
+            const template = `<source media="(min-width: 1367px)" srcset="${getImageUrl(data['slider-large'])}" />
+                    <source media="(min-width: 1000px) and (max-width: 1366)" srcset="${getImageUrl(data['slider-large'])}" />
+                    <source media="(min-width: 681px) and (max-width: 999px)" srcset="${getImageUrl(data['slider-small'])}" />
+                    <source media="(min-width: 400px) and (max-width: 680px)" srcset="${getImageUrl(data['slider-tablet'])}" />
+                    <source media="(max-width: 400px)" srcset="${getImageUrl(data['slider-mobile'])}" />
+                    <img src="${getImageUrl(data['slider-default'])}" />`;
+
+            sliderImage.innerHTML = template;
+
             // sliderImage.src = `/uploads/photos/${data[defaultImage]}`;
-        }, 1000);
+        }, 5000);
 
         contactForm.addEventListener('submit', (e) => {
             e.preventDefault();
